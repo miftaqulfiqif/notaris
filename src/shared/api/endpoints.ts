3cd5b73f7f10f095f36862efb21ff0b591a6ea7f
Rosyamdani <rosamdani91@gmail.com>
2026-02-02 16:15:21 +0700

    update using .env

--- src/shared/api/endpoints.ts
+++ src/shared/api/endpoints.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3002/api";
+export const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3002/api";
 
 export const ENDPOINTS = {
     AUTH: {
