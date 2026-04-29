915f888508ab7d8c9dcd762238216246c56a2676
Miftaqul Fiqi Firmansyah <miftaqulfiqifirmansyah131@gmail.com>
2026-04-30 00:37:07 +0700

    (fix): page reset password

--- src/app/(otp)/reset-password/page.tsx
+++ src/app/(otp)/reset-password/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
+import { Suspense } from 'react'
 import { ResetPasswordForm } from "@/features/auth/presentation/components/ResetPasswordForm";
-import { PublicOnlyRoute } from "@/features/auth/presentation/components/PublicOnlyRoute";
 
 export default function ResetPasswordPage() {
     return (
-        <div className="w-full max-w-md">
+        <Suspense fallback={<div>Loading...</div>}>
             <ResetPasswordForm />
-        </div>
+        </Suspense>
     );
 }
